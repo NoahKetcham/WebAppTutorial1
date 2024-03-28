22a100bf17588d7d91edf5fa1563d0f439a07957
NoahKetcham <150949337+NoahKetcham@users.noreply.github.com>
2024-03-28 16:23:34 -0700

    Add project files.

---
+++ WebAppTutorial1/wwwroot/js/site.js
@@ -0,0 +1,35 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function newColor() {
+    document.getElementById("hello_world").style.color = "blue";
+}
+
+function oldColor() {
+    document.getElementById("hello_world").style.color = "darkolivegreen";
+}
+
+let circle = document.getElementById('animatedCircle');
+let screenWidth = window.innerWidth;
+let speed = 2;
+let direction = 1;
+let position = 0;
+
+function animate() {
+    position += speed * direction;
+    circle.style.left = position + 'px';
+
+    if (position <= 0 || position >= screenWidth - circle.offsetWidth) {
+        direction *= -1;
+    }
+
+    requestAnimationFrame(animate);
+}
+
+
+window.onresize = function () {
+    screenwidth = window.innerwidth;
+}
+
+animate();
